b5293f70d585400df93e2ffb300ec3ee22a2483a
Freakyab <aryanbhisikar1@gmail.com>
2023-01-06 17:33:43 +0530

    testing 2

--- api/signup.js
+++ api/signup.js
@@ -10,18 +10,24 @@ const client = new MongoClient(
 //     res.json({ key: true });
 router.get("/", async (req, res) => {
     try {
+        var usernames = [];
+        var passwords = []
         console.log(req.query);
         await client.connect();
         const db = client.db("Users");
         const collection = await db.collection("AccountData").aggregate().toArray();
-        if (req.query.userName == collection.map((e,index)=>{
-            e.userName[index] 
-        })) 
-        {
-            console.log("hi");
-        }
-        console.log(username);
-        console.log(passwords);
+        collection.map((e,index)=>{
+            usernames[index] = e.userName
+            // passwords[index] = e.password
+        })
+        // if (req.query.username == usernames.map((index)=>{
+        //     usernames[index]
+        // })) 
+        // {
+            // console.log("hi");
+        // }
+        // console.log(username);
+        // console.log(passwords);
         return res.json(collection);
     } catch (error) {
         console.error(error);
