e0cff3448750cf928b9c531b8d3ab2ad6d0f168e
Freakyab <aryanbhisikar1@gmail.com>
2023-01-06 19:38:21 +0530

    added signup api

--- api/signup.js
+++ api/signup.js
@@ -4,31 +4,54 @@ const router = express.Router();
 require('dotenv').config();
 
 const client = new MongoClient(
-    process.env.DB_URL
-);
+    process.env.DB_URL, { useUnifiedTopology: true });
 
-//     res.json({ key: true });
 router.get("/", async (req, res) => {
     try {
         var usernames = [];
-        var passwords = []
-        console.log(req.query);
+        var email = [];
+        var flag = 0;
+
         await client.connect();
         const db = client.db("Users");
         const collection = await db.collection("AccountData").aggregate().toArray();
-        // collection.map((e,index)=>{
-            // usernames[index] = e.userName
-            // passwords[index] = e.password
-        // })
-        // if (req.query.username == usernames.map((index)=>{
-        //     usernames[index]
-        // })) 
-        // {
-            // console.log("hi");
-        // }
-        // console.log(username);
-        // console.log(passwords);
-        return res.json(collection);
+        const AddCollection = db.collection("AccountData")
+        
+        collection.map((e, index) => {
+            usernames[index] = e.userName
+            email[index] = e.email
+        })
+
+        usernames.map((e, index) => {
+            const UserInputEmail = email[index];
+            if (req.query.username == usernames[index]
+                && req.query.email == UserInputEmail) {
+                flag = 1;
+            }
+        })
+        
+        if (flag == 1) {
+            res.json({ key: true });
+        }
+        else {
+            // switch for insert data
+            // if (req.query.conform == "yes")
+            {
+                AddCollection.insertOne({ userName: req.query.username, email: req.query.email }, (err, result) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    console.log('Document inserted!');
+                });
+            }
+            
+            // after deleting switch delete below else 
+            // else {
+                // res.json({ key: false });
+            // }
+        }
+
     } catch (error) {
         console.error(error);
         return res.status(500).send("Server error");
