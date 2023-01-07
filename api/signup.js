90233d19c89c10859e8c498e2e8750d02e824d2c
Freakyab <aryanbhisikar1@gmail.com>
2023-01-08 00:23:45 +0530

    signup updated

--- api/signup.js
+++ api/signup.js
@@ -1,27 +1,41 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 require('dotenv').config();
 
+//connect to mongodb database
 const client = new MongoClient(
     process.env.DB_URL, { useUnifiedTopology: true });
 
+//get request
 router.get("/", async (req, res) => {
     try {
         var usernames = [];
         var email = [];
         var flag = 0;
 
+        //connect to database
         await client.connect();
         const db = client.db("Users");
         const collection = await db.collection("AccountData").aggregate().toArray();
         const AddCollection = db.collection("AccountData")
         
+        //encryption
+        const salt = await bcrypt.genSalt(10);
+        req.query.password = await bcrypt.hash(req.query.password, salt);
+        
+        //Encrypted password
+        const EnPassword = req.query.password;
+        console.log(EnPassword);
+        
+        //check the username and email
         collection.map((e, index) => {
             usernames[index] = e.userName
             email[index] = e.email
         })
 
+        //compare the username and email
         usernames.map((e, index) => {
             const UserInputEmail = email[index];
             if (req.query.username == usernames[index]
@@ -29,15 +43,18 @@ router.get("/", async (req, res) => {
                 flag = 1;
             }
         })
-        
+
+
         if (flag == 1) {
             res.json({ key: true });
         }
         else {
             // switch for insert data
             // if (req.query.conform == "yes")
+            
             {
-                AddCollection.insertOne({ userName: req.query.username, email: req.query.email ,password: req.query.password }, (err, result) => {
+                //insert data into database if username and email is not exist
+                AddCollection.insertOne({ userName: req.query.username, email: req.query.email ,password: EnPassword }, (err, result) => {
                     if (err) {
                         console.error(err);
                         return;
