2656aea06e6221861afe90d4de55a44d07f5968f
AniruddhaGawali <aniruddhagawali05@gmail.com>
2023-01-08 21:50:22 +0530

    update sign in

--- api/signup.js
+++ api/signup.js
@@ -14,6 +14,7 @@ router.get("/", async (req, res) => {
   try {
     var username = req.query.username;
     var email = req.query.email;
+    var isexist = 0;
 
     //connect to database
     await client.connect();
@@ -29,53 +30,33 @@ router.get("/", async (req, res) => {
     const EnPassword = req.query.password;
 
     //check the username and email
-    var found = collection.find((e) => {
-      console.log(e);
-      if (e === email) {
-        console.log("email found");
+    collection.find((e) => {
+      if (e.email === email) {
+        isexist = 1;
+      } else if (e.userName === username) {
+        isexist = 2;
       }
     });
-    //compare the username and email
-    // usernames.map((e, index) => {
-    //   const UserInputEmail = email[index];
-    //   if (
-    //     req.query.username == usernames[index] ||
-    //     req.query.email == UserInputEmail
-    //   ) {
-    //     flag = 1;
-    //   }
-    //   //   res.status(200).json({ key: true });
-    // });
-
-    if (flag == 1) {
-      res.json({ isSignup: false });
-    } else {
-      // switch for insert data
-      // if (req.query.conform == "yes")
-
-      {
-        //insert data into database if username and email is not exist
-        AddCollection.insertOne(
-          {
-            userName: req.query.username,
-            email: req.query.email,
-            password: EnPassword,
-            otp: null,
-          },
-          (err, result) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
+    //insert data into database if username and email is not exist
+    if (isexist == 0) {
+      AddCollection.insertOne(
+        {
+          userName: req.query.username,
+          email: req.query.email,
+          password: EnPassword,
+          otp: null,
+        },
+        (err) => {
+          if (err) {
+            res.status(500).send("Server error");
           }
-        );
-        res.status(200).json({ isSignup: true });
-      }
-
-      // after deleting switch delete below else
-      // else {
-      // res.json({ key: false });
-      // }
+        }
+      );
+      res.status(200).json({ isSignup: true });
+    } else if (isexist == 1) {
+      res.status(200).json({ isSignup: false, isEmailExist: true });
+    } else if (isexist == 2) {
+      res.status(200).json({ isSignup: false, isUsernameExist: true });
     }
   } catch (error) {
     console.error(error);
