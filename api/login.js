da0d9db0102b26a19c5f1e9366f9b4bf46715a8f
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2023-01-08 09:52:19 +0530

    Aniruddha workspace (#6)
    * add status in signup and login
    
    * update git ignore
    
    * remove env
    
    * remove packege-lock.json
    
    * update gitignore

--- api/login.js
+++ api/login.js
@@ -3,28 +3,28 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-require('dotenv').config();
+require("dotenv").config();
 
 //connect to mongodb database
-const client = new MongoClient(
-  process.env.DB_URL, { useUnifiedTopology: true });
+const client = new MongoClient(process.env.DB_URL, {
+  useUnifiedTopology: true,
+});
 
-//get request  
+//get request
 router.get("/", async (req, res) => {
   try {
     var usernames = [];
     var passwords = [];
     var flag = 0;
 
-
     await client.connect();
     const db = client.db("Users");
-    
+
     //get data from database and store in array
     const collection = await db.collection("AccountData").aggregate().toArray();
 
     //check the username and password
-    for(let i = 0; i < collection.length; i++){
+    for (let i = 0; i < collection.length; i++) {
       usernames[i] = collection[i].userName;
       passwords[i] = collection[i].password;
     }
@@ -35,16 +35,16 @@ router.get("/", async (req, res) => {
         //compare the password
         const match = await bcrypt.compare(req.query.password, passwords[i]);
         if (match) {
-
           //set the flag if username and password is correct
           flag = 1;
-          res.json({ key: true });
+          break;
         }
       }
     }
-
-    if (flag == 0) {
-      res.json({ key: false });
+    if (flag == 1) {
+      res.status(200).json({ key: true });
+    } else {
+      res.status(200).json({ key: false });
     }
   } catch (error) {
     console.error(error);
--- api/signup.js
+++ api/signup.js
@@ -2,76 +2,83 @@ const { MongoClient } = require("mongodb");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-require('dotenv').config();
+require("dotenv").config();
 
 //connect to mongodb database
-const client = new MongoClient(
-    process.env.DB_URL, { useUnifiedTopology: true });
+const client = new MongoClient(process.env.DB_URL, {
+  useUnifiedTopology: true,
+});
 
 //get request
 router.get("/", async (req, res) => {
-    try {
-        var usernames = [];
-        var email = [];
-        var flag = 0;
+  try {
+    var usernames = [];
+    var email = [];
+    var flag = 0;
 
-        //connect to database
-        await client.connect();
-        const db = client.db("Users");
-        const collection = await db.collection("AccountData").aggregate().toArray();
-        const AddCollection = db.collection("AccountData")
-        
-        //encryption
-        const salt = await bcrypt.genSalt(10);
-        req.query.password = await bcrypt.hash(req.query.password, salt);
-        
-        //Encrypted password
-        const EnPassword = req.query.password;
-        
-        //check the username and email
-        collection.map((e, index) => {
-            usernames[index] = e.userName
-            email[index] = e.email
-        })
+    //connect to database
+    await client.connect();
+    const db = client.db("Users");
+    const collection = await db.collection("AccountData").aggregate().toArray();
+    const AddCollection = db.collection("AccountData");
 
-        //compare the username and email
-        usernames.map((e, index) => {
-            const UserInputEmail = email[index];
-            if (req.query.username == usernames[index]
-                && req.query.email == UserInputEmail) {
-                flag = 1;
+    //encryption
+    const salt = await bcrypt.genSalt(10);
+    req.query.password = await bcrypt.hash(req.query.password, salt);
+
+    //Encrypted password
+    const EnPassword = req.query.password;
+
+    //check the username and email
+    collection.map((e, index) => {
+      usernames[index] = e.userName;
+      email[index] = e.email;
+    });
+
+    //compare the username and email
+    usernames.map((e, index) => {
+      const UserInputEmail = email[index];
+      if (
+        req.query.username == usernames[index] &&
+        req.query.email == UserInputEmail
+      ) {
+        flag = 1;
+      }
+    });
+
+    if (flag == 1) {
+      res.status(200).json({ key: false });
+    } else {
+      // switch for insert data
+      // if (req.query.conform == "yes")
+
+      {
+        //insert data into database if username and email is not exist
+        AddCollection.insertOne(
+          {
+            userName: req.query.username,
+            email: req.query.email,
+            password: EnPassword,
+          },
+          (err, result) => {
+            if (err) {
+              res.status(500).send("Server error");
+              return;
             }
-        })
+          }
+        );
+        res.status(200).json({ key: true });
+      }
 
-
-        if (flag == 1) {
-            res.json({ key: false });
-        }
-        else {
-            // switch for insert data
-            // if (req.query.conform == "yes")
-            
-            {
-                //insert data into database if username and email is not exist
-                AddCollection.insertOne({ userName: req.query.username, email: req.query.email ,password: EnPassword }, (err, result) => {
-                    if (err) {
-                        console.error(err);
-                        return;
-                    }
-                });
-                res.json({ key: true });
-            }
-            
-            // after deleting switch delete below else 
-            // else {
-                // res.json({ key: false });
-            // }
-        }
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Server error");
+      // after deleting switch delete below else
+      // else {
+      // res.json({ key: false });
+      // }
     }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
