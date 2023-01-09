ce2a97b69624cbb8fd91f5c6bdddfdc9c7c72a00
Freakyab <aryanbhisikar1@gmail.com>
2023-01-09 20:38:52 +0530

    update changes

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const signup = require("./api/signup");
 const login = require("./api/login");
 const forget = require("./api/forget");
 const ResetPass = require("./api/resetPassword");
-const Verfication = require("./api/Verfication");
+const Verification = require("./api/Verfication");
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-app.use("/api/signup", signup);
+app.use("/user/signup", signup);
 
 app.get("/", async (req, res) => {
   return res.status(200).send("Working");
 });
 
-app.use("/api/login", login);
-app.use("/api/forget",forget);
-app.use("/api/Verfication",Verfication);
-app.use("/api/resetPassword",ResetPass);
+app.use("/user/login", login);
+app.use("/user/forget",forget);
+app.use("/user/verification",Verification);
+app.use("/user/reset",ResetPass);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
