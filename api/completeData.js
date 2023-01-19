b5d63c5958656b3b792c098d2a9222c58de31cad
Freakyab <aryanbhisikar1@gmail.com>
2023-01-19 17:49:16 +0530

    complete data done

--- api/completeData.js
+++ api/completeData.js
@@ -1,8 +1,9 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const { json } = require("express");
 require("dotenv").config();
 
 //connect to mongodb database
@@ -11,7 +12,7 @@ const client = new MongoClient(process.env.DB_URL, {
 });
 
 //get request
-async function fetchData(username) {
+async function fetchData(userID) {
 
   const client = new MongoClient(process.env.DB_URL, { useUnifiedTopology: true });
 
@@ -20,11 +21,11 @@ async function fetchData(username) {
     const db = client.db("Users");
     const collection = await db.collection("AccountData").aggregate().toArray();
     collection.find((e) => {
-      if (e.userName === username) {
-        userID = e._id;
+      if (e._id.equals(userID)) {
+        data = e;
       }
     });
-    return userID;
+    return data;
   } catch (error) {
     console.error(error);
   }
@@ -32,13 +33,25 @@ async function fetchData(username) {
 
 router.get("/", async (req, res) => {
   try {
-    console.log(1);
+    userID = req.query.userID;
+    var flag = false;
+    // userID = "63c8335cc61868d253fca584";
     await client.connect();
     const db = client.db("Users");
     const collection = await db.collection("Password").aggregate().toArray();
-    collection.find(e=>{
-        console.log(e);
-    })
+    collection.find(e => {
+      if ((e._id).equals(userID)) {
+        JSONfile = { e };
+        flag = true;
+      }
+    }
+    )
+    if (flag) {
+      const data = await fetchData(userID);
+        mainJson = {userdata : data,data :JSONfile};
+        console.log(mainJson);
+        return res.status(200).json(mainJson);
+    }
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
